4309b44cd75abc1da74df402b0d999863ce151b5
Yash Amin <aminyash8@gmail.com>
2021-11-06 20:47:09 +0530

    Add argument parser for main

--- dbutils/__main__.py
+++ dbutils/__main__.py
@@ -1,9 +1,43 @@
 """Main"""
+import argparse
+
+from dbutils import constants
+
+
+def parse_arguments():
+    """Returns parsed known arguments."""
+    parser = argparse.ArgumentParser(add_help=False)
+
+    parser.add_argument(
+        "mode",
+        help="Enter mode",
+        choices=[
+            constants.Modes.CONVERT,
+            constants.Modes.INSERT,
+            constants.Modes.QUERY,
+        ],
+    )
+
+    parser.add_argument("options", help="Options related to mode", nargs="*")
+
+    known_args, _ = parser.parse_known_args()
+
+    return known_args
 
 
 def main():
-    """Main method"""
-    print("main")
+    """Main method
+
+    Parses arguments and runs given action.
+    """
+    args = parse_arguments()
+
+    if args.mode == constants.Modes.INSERT:
+        pass
+    elif args.mode == constants.Modes.QUERY:
+        pass
+    elif args.mode == constants.Modes.CONVERT:
+        pass
 
 
 if __name__ == "__main__":
