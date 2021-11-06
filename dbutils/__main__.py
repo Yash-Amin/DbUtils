918c18f30b6444ea20f09a94e0b416efd3fbbfa3
Yash Amin <aminyash8@gmail.com>
2021-11-06 21:55:24 +0530

    Modify main to run insert mode

--- dbutils/__main__.py
+++ dbutils/__main__.py
@@ -2,6 +2,7 @@
 import argparse
 
 from dbutils import constants
+from dbutils.actions import insert as insert_mode
 
 
 def parse_arguments():
@@ -33,7 +34,7 @@ def main():
     args = parse_arguments()
 
     if args.mode == constants.Modes.INSERT:
-        pass
+        insert_mode.run(insert_mode.create_options_from_args())
     elif args.mode == constants.Modes.QUERY:
         pass
     elif args.mode == constants.Modes.CONVERT:
